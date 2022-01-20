e465472d5f29b4d04c4b191d1b3bd6d277471927
Lin <zx@lendata.net>
2022-01-20 16:54:45 +0800

    init json response library

--- library/response/json.go
+++ library/response/json.go
@@ -26,9 +26,11 @@ func (c *jsonResponse) Error(r *ghttp.Request, status, code int, message string)
 func (c *jsonResponse) StandError(r *ghttp.Request, message string) {
 	c.Error(r, 400, 400, message)
 }
+
 func (c *jsonResponse) ServerError(r *ghttp.Request, message string) {
 	c.Error(r, 500, 500, message)
 }
+
 func (c *jsonResponse) NotFound(r *ghttp.Request) {
 	c.Error(r, 404, 404, "not found")
 }
@@ -37,7 +39,7 @@ func (c *jsonResponse) Authorization(r *ghttp.Request, message string) {
 	c.Error(r, 401, 401, message)
 }
 
-//output 设置状态码、清空buffer后输出json并退出当前业务流程
+//output 设置状态码、清空buffer后输出json并退出当前业务流程(基于gf框架，非gf框架请勿使用)
 func (c *jsonResponse) output(r *ghttp.Request, status int, code int, message string, data interface{}) {
 	r.Response.WriteStatus(status)
 	r.Response.ClearBuffer()
